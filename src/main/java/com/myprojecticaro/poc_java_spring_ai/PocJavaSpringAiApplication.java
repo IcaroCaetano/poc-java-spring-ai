8c307f4ec8ad104aa01dddae9310d04304f72122
IcaroCaetano <icaro.caetano@gmail.com>
2026-02-25 22:06:31 -0300

    first commit

---
+++ src/main/java/com/myprojecticaro/poc_java_spring_ai/PocJavaSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_java_spring_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJavaSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJavaSpringAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/poc_java_spring_ai/PocJavaSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_java_spring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJavaSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
