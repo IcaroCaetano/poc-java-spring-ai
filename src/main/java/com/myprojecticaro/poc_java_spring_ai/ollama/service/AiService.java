41bd5e2c2a889a6f691812c2d76769a089283216
IcaroCaetano <icaro.caetano@gmail.com>
2026-03-03 22:58:47 -0300

    feat: add  spring ai client chat

--- src/main/java/com/myprojecticaro/poc_java_spring_ai/ollama/service/AiService.java
+++ src/main/java/com/myprojecticaro/poc_java_spring_ai/ollama/service/AiService.java
@@ -1,6 +1,8 @@
 package com.myprojecticaro.poc_java_spring_ai.ollama.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,9 +10,12 @@ public class AiService {
 
     private final ChatClient chatClient;
 
-    public AiService(ChatClient.Builder builder) {
+    private final ChatMemory chatMemory;
 
+
+    public AiService(ChatClient.Builder builder, ChatMemory chatMemory) {
         this.chatClient = builder.build();
+        this.chatMemory = chatMemory;
     }
 
     public String ask(String question) {
@@ -22,6 +27,23 @@ public class AiService {
                          Se possível, inclua exemplos práticos.
                         """)
                 .user(question)
+                .call()
+                .content();
+    }
+
+    public String askv2(String conversationId, String question) {
+
+        return chatClient.prompt()
+                .system("""
+                    Você é um especialista em Java e Spring.
+                    Use o contexto da conversa quando necessário.
+                    """)
+                .user(question)
+                .advisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory)
+                                .conversationId(conversationId)
+                                .build()
+                )
                 .call()
                 .content();
     }
